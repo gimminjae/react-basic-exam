c37113a85e0d684d714311edfd13636c238aede5
gimminjae <min356812@naver.com>
2024-03-21 11:05:39 +0900

    react exercise: AppCounter, AppProducts, AppJSX, AppCounter

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import AppToDoList from './AppToDoList';
+// import AppCounter from './AppCounter';
+// import AppJSX from './AppJSX';
+// import AppProfile from './AppProfile';
+// import AppProducts from './AppProducts';
+
+export default function App() {
+
+  return (
+    <>
+    {/* <AppToDoList /> */}
+    <AppProfile />
+    <hr />
+    <AppProducts />
+    <hr />
+    <AppJSX />
+    <hr />
+    <AppCounter />
+    </>
+  );
+}
